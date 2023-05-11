71475b0b86c996425102a8721a586c7008fe15b3
devcsb <devcsb119@gmail.com>
2023-05-11 14:20:06 +0900

    스프링부트 자세히 살펴보기 - 자동구성 조건 결과 확인
    ConditionEvaluationReport를 활용하여 자동구성 조건 결과를 확인

---
+++ src/test/java/com/example/springbootautocofig/SpringbootAutocofigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootautocofig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAutocofigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
